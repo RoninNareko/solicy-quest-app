4103b4c4064728d66e87887d14458b45c604c44e
narekhasratyan <hasratyan.2000@gmail.com>
2022-10-05 23:30:25 +0400

    pre relase version

--- frontend/src/components/AccountInfoPageComponents/InfoTable.tsx
+++ frontend/src/components/AccountInfoPageComponents/InfoTable.tsx
@@ -9,31 +9,31 @@ const InfoTable: React.FC<IProps> = ({ accounts }) => {
   const accountData = accounts[0];
   return (
     <section>
-      <Descriptions layout="vertical">
+      <Descriptions bordered layout="horizontal">
         <Descriptions.Item>
           <b>ID</b>
         </Descriptions.Item>
         <Descriptions.Item>{accountData.id}</Descriptions.Item>
       </Descriptions>
-      <Descriptions>
+      <Descriptions bordered layout="horizontal">
         <Descriptions.Item>
           <b>Name</b>
         </Descriptions.Item>
         <Descriptions.Item>{accountData.name}</Descriptions.Item>
       </Descriptions>
-      <Descriptions>
+      <Descriptions bordered layout="horizontal">
         <Descriptions.Item>
           <b>Owner</b>
         </Descriptions.Item>
         <Descriptions.Item>{accountData.owner}</Descriptions.Item>
       </Descriptions>
-      <Descriptions>
+      <Descriptions bordered layout="horizontal">
         <Descriptions.Item>
           <b>Created On</b>
         </Descriptions.Item>
         <Descriptions.Item>{accountData.createdOn}</Descriptions.Item>
       </Descriptions>
-      <Descriptions>
+      <Descriptions bordered layout="horizontal">
         <Descriptions.Item>
           <b>Updated On</b>
         </Descriptions.Item>
